fd2acd43ac6ff4b303a3f85f021378fb47ef2a5c
Tran Ngoc Luan <44539564+trngocluan@users.noreply.github.com>
2025-07-15 14:31:51 +0700

    Bật CORS (Cross-Origin Resource Sharing) cho phép ứng dụng frontend (Angular) gọi API từ backend (NestJS) mà không bị chặn bởi trình duyệt. Trên main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  // ✅ Tạo ứng dụng NestJS với CORS được bật
+  // CORS (Cross-Origin Resource Sharing) cho phép ứng dụng frontend (Angular) gọi API từ backend (NestJS) mà không bị chặn bởi trình duyệt.
+  // Việc bật CORS ở đây sẽ cho phép frontend và backend giao tiếp với nhau
+  const app = await NestFactory.create(AppModule, { cors: true }); // ✅ bật CORS ở đây
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
